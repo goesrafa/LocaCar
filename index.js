c0c9d8569807582ec7a38adbf97ea36b76839880
goesrafa <62679926+goesrafa@users.noreply.github.com>
2021-05-17 21:25:32 -0300

    Add Grupos

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ require('dotenv').config() //Carregamento das 'variaveis de ambientes'
 const InicializaMongoServer = require('./config/Db')
 //Definição das rotas da aplicação
 const rotasCategoria = require('./routes/Categoria')
+const rotasGrupo = require('./routes/Grupos')
+const rotasUpload = require('./routes/Upload')
 
 //Inicializando o servidor MongoDB
 InicializaMongoServer()
 
 const app = express()
+
+//Removendo por segurança
+app.disable('x-powered-by')
+
 //PORTA DEFAULT 
 const PORT = process.env.PORT
+
+//Middleware do Express
+app.use(function(req, res, next){
+    //Em produção, remova o * e atualize com o domínio/ip do seu app
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    //Cabeçalhos que serão permitidos
+    res.setHeader('Access-Control-Allow-Headers','*')
+    //Ex: res.setHeader('Access-Control-Allow-Headers','Content-Type, Accept, access-token')
+    //Métodos que serão permitidos
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH')
+    next()
+})
+
 //Parse conteudo JSON
 app.use(express.json())
 
@@ -19,6 +38,16 @@ app.get('/', (req, res) =>{
 })
 /*Rotas da Categoria */
 app.use('/categorias', rotasCategoria)
+/*Rotas do Grupo */
+app.use('/grupos', rotasGrupo)
+app.use('/public', express.static('public'))
+/* Rota do upload */
+app.use('/upload', rotasUpload)
+
+/* Rota para tratar exceções - 404 (Deve ser a última rota SEMPRE) */
+app.use(function(req, res) {
+    res.status(404).json({message: `A rota ${req.originalUrl} não existe`})
+})
 
 app.listen(PORT, (req, res) =>{
     console.log(`💻Servidor Web iniciado na porta ${PORT}`)
--- routes/Grupos.js
+++ routes/Grupos.js
@@ -37,8 +37,104 @@ router.get('/:id', async(req, res)=> {
     }
 })
 
+/********************************************
+ * Lista um grupo pelo id da Categoria
+ * GET /grupos/categoria/:id
+*********************************************/
+router.get('/categoria/:id', async(req, res)=>{
+    try{
+        const grupos = await Grupos
+        .find({"categoria": req.params.id})
+        .sort({nome: 1})
+        .populate("categorias", "nome")
+    res.json(grupos)
+    }catch(err) {
+        res.status(500).send({
+            errors: [{message: `Não foi possível obter o grupo com o id da categoria ${req.params.id}`}]
+        })
+    }
+})
 
+/********************************************
+ * Incluir um novo grupo
+ * POST /grupos
+*********************************************/
+const validarGrupo = [
+    check('nome')
+    .not().isEmpty().withMessage('Nome do grupo é obrigatório!')
+    .isLength({min: 5}).withMessage('Nome do grupo é muito curto'),
+    check('status', 'Informe um status válido para o grupo.').isIn(['ativo', 'inativo']),
+    check('notaMedia')
+    .isNumeric().withMessage('A nota média deve ser um número'),
+    check('categoria')
+    .isMongoId().trim().withMessage('A categoria do grupo é inválida!'),
+    check('faixaPreco', 'A faixa de preço informada é inválida').isIn(['economico', 'intermediario', 'executivo'])
+]
 
+router.post('/', validarGrupo, async(req, res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+//Verificação se o grupo já existe
+    const { nome } = req.body
+    let grupo = await Grupos.findOne({nome})
+    if(grupos)
+        return res.status(200).json({
+            errors: [{message: 'Já existe um grupo com o nome informado'}]
+        })
+    try{
+        let grupo = new Grupos(req.body)
+        await grupo.save()
+        res.send(grupo)
+    }catch (err){
+        return res.status(500).json({
+            errors: [{message: `Erro ao salvar o grupo: ${err.message}`}]
+        })
+    }
+})
+
+/********************************************
+ * Remove um grupo
+ * DELETE /grupos/:id
+*********************************************/
+router.delete('/:id', async(req, res)=>{
+    await Grupo.findByIdAndRemove(req.params.id)
+    .then(grupo =>{
+        res.send({message: `Grupo ${grupo.nome} removido com sucesso!`})
+    }).catch(err =>{
+        return res.status(500).send({
+            errors: [{message: `Não foi possível apagar o grupo com o id ${req.params.id}`}]
+        })
+    })
+})
+
+/********************************************
+ * Edita um grupo
+ * PUT /grupos
+*********************************************/
+router.put('/', validarGrupo, async(req, res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+    let dados = req.body
+    await Grupo.findByIdAndUpdate(req.body._id, {
+        $set: dados
+    }, {new: true})
+    .then(grupo =>{
+        res.send({message: `Grupo ${grupo.nome} alterado com sucesso!`})
+    }).catch(err => {
+        return res.status(500).send({
+            errors: [{message: 
+           `Não foi possível alterar o grupo com o id ${req.body._id}`}]
+        })
+    })
+})
 
 
 module.exports = router
