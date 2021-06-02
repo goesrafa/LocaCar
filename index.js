f99444bcfc2f442fee01198c6237d30f08ccd802
goesrafa <62679926+goesrafa@users.noreply.github.com>
2021-06-02 13:54:10 -0300

    Nova Versão

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.json())
 
 app.get('/', (req, res) =>{
     res.json({mensagem: "API LocaCar 100% funcional!!🤙 ",
-            versao: '1.0.8'})
+            versao: '1.0.9'})
 })
 /*Rotas da Categoria */
 app.use('/categorias', rotasCategoria)
